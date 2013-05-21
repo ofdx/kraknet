7bef3148b4508931b53a7154c0801d259225671b
krakissi <mike@krakissi.net>
2013-05-21 13:30:05 -0400

    Web directory no longer needed.
    The default web/index.html is now created when the quick build script is
    run, if no other index.html exists.

--- build.bash
+++ build.bash
@@ -6,6 +6,24 @@ error_level=0
 inc_error(){
 	error_level=$(expr $error_level + 1)
 }
+create_default_doc(){
+cat > "web/index.html" << EOF
+!# Welcome, traveler!
+!# HTTP headers can be sent to clients here.
+!# Leave comments by starting with a #.
+!Cache-Control: no-cache
+<!DOCTYPE html>
+<html><head>
+	<meta charset=UTF-8>
+	<title>Welcome to Kraknet!</title>
+</head><body>
+	<h2>It works!</h2>
+	<p>The server is running, but no content is available yet. The current time is <????time:time>.</p>
+	<p><i>Kraknet Site System &copy;2012-<????time:year></i></p>
+	<textarea><????sys:env></textarea>
+</body></html>
+EOF
+}
 
 cat << EOF
 This script will rebuild the entire site system. If you want to do something
@@ -39,6 +57,7 @@ fi
 
 # Start build process.
 error_level=0
+opwd=$PWD
 cd src
 
 echo "-- Deleting old binaries."
@@ -49,8 +68,24 @@ if ! make; then
 	inc_error
 fi
 
+cd $opwd
+
+# Build web/index.html if it doesn't exist.
+if ! stat "web/index.html" > /dev/null 2>&1; then
+	echo "-- No web/index.html, adding default."
+	mkdir -p web
+	create_default_doc
+fi
+
 # Error report.
 cat << EOF
 
 Done. ($error_level build errors)
 EOF
+
+if [ "$error_level" -eq "0" ]; then
+cat << EOF
+Build completed successfully. You can start the server now with
+	./init_ws start
+EOF
+fi
