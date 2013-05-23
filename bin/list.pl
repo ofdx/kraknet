36bee350b0b7016c7db07a50ed81591ec3e4da3d
Mike <mike@krakissi.net>
2013-05-22 20:42:00 -0400

    Added list.pl as an alternative to list.bash
    list.pl is now the default directory listing script. If Perl is not
    available in your environment, you can reset the symlink for bin/list
    to list.bash.

---
+++ bin/list.pl
@@ -0,0 +1,86 @@
+#!/usr/bin/perl -w
+# Perl directory listing script for Krakws.
+# Slightly less temporary solution.
+#
+# Mike Perron (2013)
+
+use File::stat;
+use Switch;
+
+$working=$ENV{'web_root'};
+
+# Find the present working directory.
+$dir=`dirname "$ENV{'SCRIPT_NAME'}"`;
+chomp $dir;
+
+if($dir eq "/"){
+	$dirf="$dir";
+} else {
+	$dirf="$dir/";
+}
+
+print<<EOF;
+Content-Type: Text/html
+
+<!DOCTYPE html>
+<html><head>
+	<meta charset=UTF-8>
+	<title>Index of $dirf</title>
+	<style type=text/css>
+		body { font-family: sans-serif; }
+		td { padding-right: 5px; }
+		th { text-align: left; }
+		.size { text-align: right; font-family: monospace; }
+		tr:nth-child(even) { background-color:#e0e0e0; }
+	</style>
+</head><body>
+	<h2>Files located in $dirf</h2>
+	<table>
+		<tr>
+			<th>Name</th><th>Size (Bytes)</th><th>Description</th>
+		</tr>
+EOF
+
+# If the directory is the root dir, just clear it.
+# No other directory looks like /\/$/
+if($dir eq "/"){
+	$dir="";
+}
+
+# Loop to generate each row.
+$dirname="$working/$dir";
+opendir my($dh), $dirname or die "Couldn't read directory. ($dirname): $!";
+my @file=readdir $dh;
+closedir $dh;
+@file=sort @file;
+
+foreach (@file){
+	$sb=stat("$working/$dir/$_");
+	$size=$sb->size;
+	$mime=`file -b "$working/$dir/$_"`;
+	chomp $mime;
+
+	if($mime eq "directory"){
+		$filef="$_/";
+		$size="";
+		switch($_){
+			case ".." { $mime="<b>Move Up</b>"; }
+			case "." { $mime="<b>Present Directory</b>"; }
+		}
+	} else {
+		$filef="$_";
+	}
+
+print <<EOF;
+		<tr>
+			<td><a href="$dir/$_">$filef</a></td>
+			<td class=size>$size</td>
+			<td>$mime</td>
+		</tr>
+EOF
+}
+
+print <<EOF;
+	</table>
+</body></html>
+EOF
