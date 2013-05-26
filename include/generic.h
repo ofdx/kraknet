90a4095d5aff11eb87e31e5ef027b6fe496256c3
Mike <mike@krakissi.net>
2013-05-26 13:40:22 -0400

    Moved actual location of headers.
    Headers which have their object code built into the Kraknet shared
    library are now located at /include/ with symlinks at /src/i/.
    Additionally, a help file /include/README was added to hint at how to
    use functions from libkraknet.

--- include/generic.h
+++ include/generic.h
@@ -1,1 +1,40 @@
-../src/i/generic.h
+/*
+	generic
+	Written by Mike Perron (2012-2013)
+
+	This Source Code Form is subject to the terms of the Mozilla Public
+	License, v. 2.0. If a copy of the MPL was not distributed with this
+	file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+	General functions to patch things together for krakws.
+*/
+
+#ifndef KWS_GENERIC_F
+#define KWS_GENERIC_F
+
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+enum SEEK_MODE {
+	SEEK_FORWARD_ONLY,
+	SEEK_RESET_OK,
+	SEEK_POST_REWIND // Do not use.
+};
+
+#define WORDS_DELIMINATOR " \t\r\n"
+
+// String functions
+extern char **chop_words(const char *src);
+extern void unescape_url(char *url);
+extern char x2c(char *what);
+extern void sanitize_str(char *str);
+extern void unquote_str(char *str);
+
+// Config file parsing
+extern FILE *get_conf_stream(char *cname, const char *mode);
+extern char *get_conf_line(char *fname,char *value);
+extern char *get_conf_line_s(FILE *stream, char *value, enum SEEK_MODE mode);
+
+#endif
