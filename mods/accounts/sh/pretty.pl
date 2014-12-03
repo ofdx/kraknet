ece938b1ae673c0744033dcd09317915fa4a4e87
krakissi <mike@krakissi.net>
2014-12-03 17:11:50 -0500

    Script to return display names for a given user

---
+++ mods/accounts/sh/pretty.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+use strict;
+use DBI;
+
+my $dbh = DBI->connect('dbi:mysql:kws', 'kraknet', '') or &fail("could not access DB");
+
+my $user = $ARGV[0];
+my $sql = qq/SELECT pretty FROM users WHERE name = ?;/;
+my $sth = $dbh->prepare($sql);
+$sth->execute($user);
+
+my @row = $sth->fetchrow_array();
+if($row[0]){
+	my $pretty = $row[0];
+
+	printf "$pretty";
+	exit 0
+} else {
+	die "User not found";
+}
